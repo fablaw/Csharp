e84c3628c4fe16e99ffd84159f507dd10e6be75a
Fabian Law <fbn_lw@hotmail.com>
2023-03-24 12:00:35 +0100

    Directory.EnumerateFiles function

--- program/program.cs
+++ program/program.cs
@@ -27,6 +27,57 @@ namespace ConsoleApplication
       {
         File.Create(topPath+$"/testEduGroupe/{names[j]}.txt");
       }
+      /*
+      string[] arrFiles=Directory.GetFiles(topPath);
+      string[] arrDirectory=Directory.GetDirectories(topPath);
+      foreach (string file in arrFiles)
+      {
+        Console.WriteLine(file);
+      }
+      Console.WriteLine("==================================");
+      foreach (string dk in arrDirectory)
+      {
+        Console.WriteLine(dk);
+      }
+      Console.WriteLine(arrDirectory.Length);
+      Console.WriteLine(arrDirectory[0]);
+
+      if (arrFiles.Length==0)
+      {
+        Console.WriteLine($"There are no files in {topPath}");
+      }
+      else
+      {
+        Console.WriteLine($"These files exist in {topPath}.");
+        foreach (string filename in arrFiles)
+        {
+          if (filename.EndsWith(".txt"))
+          {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(filename));
+          }
+        }
+      }
+      foreach (string dk in arrDirectory)
+      {
+        Console.WriteLine($"These files exist in {dk}.");
+        string[] subfiles=Directory.GetFiles(dk);
+        foreach (string subfile in subfiles)
+        {
+          if (subfile.EndsWith(".txt"))
+          {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(subfile));
+          }
+        }
+        string[] subdk=Directory.GetDirectories(dk);
+        if (subdk.Length==0)
+        {
+          Console.WriteLine($"There are no subdirectories in {dk}.");
+        }
+      }*/
+      foreach (string file in Directory.EnumerateFiles(topPath, "*.txt", SearchOption.AllDirectories))
+      {
+        Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+      }
     }
   }
 }
